7dde9aeadf030c99629f3ed55a3298237a39fc40
amyrogan <roganamy@gmail.com>
2023-03-20 17:41:46 -0500

    Interactions
    recommitting previous

--- js/index.js
+++ js/index.js
@@ -4,13 +4,16 @@ $(".burger").on("click", function() {
     console.log("you clicked on hamburger!");
 });
 
+
 $(".burger").on("click", function() {
     $(".dropDown").fadeToggle("400");
 });
 
+
 $(".burger").change (function() {
     $(".burger").change(".close");
 });
+
 
 $(".close").on("click", function() {
     $(".dropDown").fadeToggle("200");
